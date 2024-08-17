919e3b03b9fde9ab1d22cb0876c68d5b24851abe
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-08-17 11:00:55 -0300

    chore: add create account controller

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, HttpCode, Post } from '@nestjs/common'
+import { ConflictException } from '@nestjs/common'
+
+import { PrismaService } from '../prisma/prisma.service'
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(
+    @Body() body: { email: string, name: string, password: string },
+  ) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        'User with same e-mail address already exists.',
+      )
+    }
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    })
+  }
+}
