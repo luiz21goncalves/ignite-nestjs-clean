2121e21fa5a6f7945f407ef405d3ee7e03db4dc1
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-08-17 14:30:05 -0300

    feat: create new question

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import {
   ApiBadRequestResponse,
   ApiBearerAuth,
@@ -6,23 +6,58 @@ import {
   ApiInternalServerErrorResponse,
   ApiTags,
 } from '@nestjs/swagger'
+import { z } from 'zod'
 
 import { CurrentUser } from '../auth/current-user-decorator'
 import { UserPayload } from '../auth/jwt.strategy'
 import { JwtAuthGuard } from '../auth/jwt-auth.guard'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { PrismaService } from '../prisma/prisma.service'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
 @ApiBearerAuth()
 @ApiTags('Questions')
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
+  constructor(private readonly prisma: PrismaService) {}
+
   @Post()
   @ApiCreatedResponse()
   @ApiBadRequestResponse()
   @ApiInternalServerErrorResponse()
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log(user)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Body(new ZodValidationPipe(createQuestionBodySchema))
+    body: CreateQuestionBodySchema,
+  ) {
+    const { content, title } = body
+    const slug = this.convertToSlug(title)
 
-    return 'ok'
+    const question = await this.prisma.question.create({
+      data: {
+        content,
+        title,
+        slug,
+        authorId: user.sub,
+      },
+    })
+
+    return { question }
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-')
   }
 }
