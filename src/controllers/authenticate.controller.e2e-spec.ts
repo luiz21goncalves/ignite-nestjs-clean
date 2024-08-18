87c680ef48099ed2e0971c7cbccae4c4ac0dc5bf
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-08-18 10:39:39 -0300

    test: authenticate controller

---
+++ src/controllers/authenticate.controller.e2e-spec.ts
@@ -0,0 +1,50 @@
+import { faker } from '@faker-js/faker'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { hash } from 'bcryptjs'
+import request from 'supertest'
+import { beforeAll, describe, expect, test } from 'vitest'
+
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+
+describe('Authenticate (e2e)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[post] /sessions', async () => {
+    const name = faker.person.fullName()
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+    const hashedPassword = await hash(password, 2)
+
+    await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+      },
+    })
+
+    const response = await request(app.getHttpServer()).post('/sessions').send({
+      email,
+      password,
+    })
+
+    expect(response.statusCode).toBe(201)
+    expect(response.body).toStrictEqual({
+      access_token: expect.any(String),
+    })
+  })
+})
